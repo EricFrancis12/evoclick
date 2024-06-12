3ce9f5c049b14336c0881cdd45e9338691c6bac6
Eric <ericfrancis1@proton.me>
2024-06-12 15:27:46 -0500

    now using single quotes on .ts and .js

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,15 +1,15 @@
-"use server";
+'use server';
 
-import prisma from "./db";
+import prisma from './db';
 
 export async function CreateNewLinkAction(formData: FormData) {
     await prisma.link.create({
         data: {
-            category: formData.get("category")?.toString() || "",
-            description: formData.get("description")?.toString() || "",
-            imageUrl: formData.get("imageUrl")?.toString() || "",
-            title: formData.get("title")?.toString() || "",
-            url: formData.get("url")?.toString() || "",
+            category: formData.get('category')?.toString() || '',
+            description: formData.get('description')?.toString() || '',
+            imageUrl: formData.get('imageUrl')?.toString() || '',
+            title: formData.get('title')?.toString() || '',
+            url: formData.get('url')?.toString() || '',
         }
     });
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,6 +11,6 @@ declare global {
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     globalThis.prismaGlobal = prisma;
 }
