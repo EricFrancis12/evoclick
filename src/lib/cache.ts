df26995ffa97889478fcae176c6932859f4acc73
Eric <ericfrancis1@proton.me>
2024-06-16 09:28:47 -0500

    implimented basic redis cache

---
+++ src/lib/cache.ts
@@ -0,0 +1,27 @@
+import { createClient, RedisClientType } from 'redis';
+
+// Using the redis cache is an optional feature
+let cache: RedisClientType | undefined;
+
+// If a REDIS_URL is provided, a redis client will be created
+if (process.env.REDIS_URL) {
+    cache = createClient({
+        url: process.env.REDIS_URL,
+        socket: {
+            reconnectStrategy: 5000 // If disconnected, try to reconnect every 5 seconds
+        },
+        disableOfflineQueue: true // If disconnected, commands are NOT queued, and are instead rejected
+    });
+}
+
+cache?.on('connect', () => console.log('Connected to Redis'));
+cache?.on('ready', () => console.log('Redis client is ready to use'));
+cache?.on('end', () => console.log('Redis connection has been closed'));
+cache?.on('reconnecting', () => console.log('Redis client is attempting to reconnect to server'));
+cache?.on('error', err => console.error('Redis error:', err));
+
+if (cache && !cache.isOpen) {
+    cache.connect();
+}
+
+export default cache;
--- src/lib/data/affiliateNetworks.ts
+++ src/lib/data/affiliateNetworks.ts
@@ -1,20 +1,55 @@
+import { z } from 'zod';
 import prisma from '../db';
+import cache from '../cache';
 import { IAffiliateNetwork, IAffiliateNetwork_createRequest, IAffiliateNetwork_updateRequest } from '../types';
+
+const affiliateNetworkSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    defaultNewOfferString: z.string(),
+    tags: z.array(z.string()),
+    createdAt: z.date(),
+    updatedAt: z.date()
+});
+
+type TAffiliateNetwork = z.infer<typeof affiliateNetworkSchema>;
 
 export async function getAllAffiliateNetworks(): Promise<IAffiliateNetwork[]> {
     return prisma.affiliateNetwork.findMany();
 }
 
 export async function getAffiliateNetworkById(id: number): Promise<IAffiliateNetwork | null> {
-    return prisma.affiliateNetwork.findUnique({
+    // Check redis cache for this affiliate network
+    const key = `affiliateNetwork:${id}`;
+    const cachedResult = await cache?.get(key);
+    console.log(typeof cachedResult === 'string' ? 'hit' : 'miss');
+
+    // If found in the cache, parse and return it
+    const { data, success } = affiliateNetworkSchema.safeParse(cachedResult);
+    if (success) return data;
+
+    // If not in cache, query db for it
+    const affiliateNetworkProm = prisma.affiliateNetwork.findUnique({
         where: { id }
     });
+
+    // If we fetch from the db successfully, create a new key for this affiliate network in the cache
+    affiliateNetworkProm.then(affiliateNetwork => {
+        if (affiliateNetwork && cache) {
+            cache.set(key, JSON.stringify(affiliateNetwork), {
+                EX: 60 // Exipry time in seconds
+            });
+        }
+    });
+
+    return affiliateNetworkProm;
 }
 
 export async function createNewAffiliateNetwork(affNetReqest: IAffiliateNetwork_createRequest): Promise<IAffiliateNetwork> {
     return prisma.affiliateNetwork.create({
         data: { ...affNetReqest }
     });
+    // if the creation was successful, create a new key for this new affiliate network in the cache
 }
 
 export async function updateAffiliateNetworkById(id: number, data: IAffiliateNetwork_updateRequest): Promise<IAffiliateNetwork> {
@@ -22,10 +57,12 @@ export async function updateAffiliateNetworkById(id: number, data: IAffiliateNet
         where: { id },
         data
     });
+    // if the update was successful, update the corresponding key for this affiliate network in the cache
 }
 
 export async function deleteAffiliateNetworkById(id: number): Promise<IAffiliateNetwork> {
     return prisma.affiliateNetwork.delete({
         where: { id }
     });
+    // if the deletion was successful, delete the corresponding key for this affiliate network in the cache
 }
