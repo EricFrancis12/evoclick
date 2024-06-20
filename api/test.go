4a743f3bd3bbef2640882aeec26d225278469524
Eric <ericfrancis1@proton.me>
2024-06-20 16:54:37 -0500

    modified /api/test.go

--- api/test.go
+++ api/test.go
@@ -1,54 +1,59 @@
 package handler
 
 import (
-	"context"
+	// "context"
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	"net/http"
 
 	"github.com/EricFrancis12/evoclick/pkg"
-	"github.com/EricFrancis12/evoclick/prisma/db"
+	// "github.com/EricFrancis12/evoclick/prisma/db"
 )
 
 func Test(w http.ResponseWriter, r *http.Request) {
-	client := db.NewClient()
-	if err := client.Prisma.Connect(); err != nil {
-		fmt.Errorf(err.Error())
-		return
-	}
+	/*
+		Example of how to read & write to db commented out below
+		to avoid abuse on deploy
+	*/
 
-	defer func() {
-		if err := client.Prisma.Disconnect(); err != nil {
-			panic(err)
-		}
-	}()
+	// client := db.NewClient()
+	// if err := client.Prisma.Connect(); err != nil {
+	// 	fmt.Errorf(err.Error())
+	// 	return
+	// }
 
-	ctx := context.Background()
+	// defer func() {
+	// 	if err := client.Prisma.Disconnect(); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
 
-	// create a affiliate network
-	createdAffiliateNetwork, err := client.AffiliateNetwork.CreateOne(
-		db.AffiliateNetwork.Name.Set("Hi from Prisma!"),
-		db.AffiliateNetwork.DefaultNewOfferString.Set("123"),
-	).Exec(ctx)
-	if err != nil {
-		fmt.Errorf(err.Error())
-		return
-	}
+	// ctx := context.Background()
 
-	result, _ := json.MarshalIndent(createdAffiliateNetwork, "", "  ")
-	fmt.Printf("created affiliateNetwork: %s\n", result)
+	// // create a new affiliate network
+	// createdAffiliateNetwork, err := client.AffiliateNetwork.CreateOne(
+	// 	db.AffiliateNetwork.Name.Set("Hi from Prisma!"),
+	// 	db.AffiliateNetwork.DefaultNewOfferString.Set("123"),
+	// ).Exec(ctx)
+	// if err != nil {
+	// 	fmt.Errorf(err.Error())
+	// 	return
+	// }
 
-	// find a single affiliate network
-	affiliateNetwork, err := client.AffiliateNetwork.FindUnique(
-		db.AffiliateNetwork.ID.Equals(createdAffiliateNetwork.ID),
-	).Exec(ctx)
-	if err != nil {
-		fmt.Errorf(err.Error())
-		return
-	}
+	// result, _ := json.MarshalIndent(createdAffiliateNetwork, "", "  ")
+	// fmt.Printf("created affiliateNetwork: %s\n", result)
 
-	result, _ = json.MarshalIndent(affiliateNetwork, "", "  ")
-	fmt.Printf("affiliateNetwork: %s\n", result)
+	// // find a single affiliate network
+	// affiliateNetwork, err := client.AffiliateNetwork.FindUnique(
+	// 	db.AffiliateNetwork.ID.Equals(createdAffiliateNetwork.ID),
+	// ).Exec(ctx)
+	// if err != nil {
+	// 	fmt.Errorf(err.Error())
+	// 	return
+	// }
+
+	// result, _ = json.MarshalIndent(affiliateNetwork, "", "  ")
+	// fmt.Printf("affiliateNetwork: %s\n", result)
 
 	jsonEncoder := json.NewEncoder(w)
 	jsonEncoder.SetIndent("", "  ")
@@ -57,7 +62,7 @@ func Test(w http.ResponseWriter, r *http.Request) {
 		Path:         r.URL.Path,
 		QueryStrings: r.URL.Query(),
 		Method:       r.Method,
-		Message:      "Hello from ./api/t.go",
+		Message:      "Hello from ./api/test.go",
 	}
 
 	if err := jsonEncoder.Encode(debugResponse); err != nil {
