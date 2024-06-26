8fb1e22bda98b0169ed914893e72c3a74aa34843
Eric <ericfrancis1@proton.me>
2024-06-25 23:33:56 -0500

    WIP: started /postback route handler

--- api/postback.go
+++ api/postback.go
@@ -1,24 +1,59 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/EricFrancis12/evoclick/pkg"
 )
 
+type PostbackResp struct{}
+
 func Postback(w http.ResponseWriter, r *http.Request) {
+	timestamp := time.Now()
+	ctx := context.Background()
+	storer := pkg.NewStorer()
+	storer.Renew()
+
+	clickPublicId := r.URL.Query().Get("cid")
+	if clickPublicId != "" {
+		// Get click from db
+		click, err := storer.GetClickByPublicId(ctx, clickPublicId)
+		if err != nil {
+			fmt.Println("error fetching click by Public ID: " + err.Error())
+		} else {
+			// Update click in db
+			revenue := getRevenue(*r.URL)
+			convertedClick := convertClick(click, timestamp, revenue)
+			if _, err := storer.UpsertClickById(ctx, click.ID, convertedClick); err != nil {
+				fmt.Println("error updating click in db: " + err.Error())
+			}
+		}
+	}
+
 	jsonEncoder := json.NewEncoder(w)
-	jsonEncoder.SetIndent("", "  ")
-
-	debugResponse := &Response{
-		Path:         r.URL.Path,
-		QueryStrings: r.URL.Query(),
-		Method:       r.Method,
-		Message:      "Hello from ./api/postback.go",
-	}
-
-	err := jsonEncoder.Encode(debugResponse)
+	err := jsonEncoder.Encode(PostbackResp{})
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		fmt.Println("error encoding JSON: " + err.Error())
 	}
+}
+
+func convertClick(click pkg.Click, convTime time.Time, revenue int) pkg.Click {
+	click.ConvTime = convTime
+	click.Revenue += revenue
+	return click
+}
+
+func getRevenue(url url.URL) int {
+	payoutStr := url.Query().Get("payout")
+	revenue, err := strconv.Atoi(payoutStr)
+	if err != nil || revenue < 0 {
+		return 0
+	}
+	return revenue
 }
