3c4d901cc2cc76b0314b332615eef5b9abbc4d9e
Eric <ericfrancis1@proton.me>
2024-07-21 10:45:45 -0500

    added pending state to RefreshButton

--- src/components/RefreshButton.tsx
+++ src/components/RefreshButton.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
-import { revalidatePathAction } from "@/lib/actions";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { faSpinner, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
 
 export default function RefreshButton({ disabled = false }: {
     disabled?: boolean;
 }) {
+    const router = useRouter();
+    const [isPending, startTransition] = useTransition();
+
+    function handleClick() {
+        startTransition(() => {
+            // Refresh the current route and fetch new data from the server without
+            // losing React state.
+            router.refresh();
+        });
+    }
+
     return (
         <Button
-            icon={faSyncAlt}
-            onClick={() => revalidatePathAction(window.location.href)}
-            disabled={disabled}
+            icon={isPending ? faSpinner : faSyncAlt}
+            onClick={handleClick}
+            disabled={disabled || isPending}
             text="Refresh"
         />
     )
